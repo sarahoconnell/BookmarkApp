3f59b4b71eb87f8465478616e1d787fd1070803d
sarahoconnell <sarahoconnell@gmail.com>
2013-04-24 15:04:19 +0100

    Missing conatants file

---
+++ bookmark-app/src/main/java/ie/cit/adf/constants/Constants.java
@@ -0,0 +1,52 @@
+package ie.cit.adf.constants;
+
+public class Constants {
+
+	    // RequestMappings
+		public static final String redirect = "redirect:";
+		public static final String registerUserMapping = "/registerUser";
+		public static final String createUserMapping = "/createUser";
+		public static final String adminMapping = "/admin";
+		public static final String removeUserMapping = "/removeUser";
+		public static final String toggleUserEnableMapping = "/toggleUserEnable";
+		public static final String indexMapping = "/index";
+		public static final String showPublicBoardsMapping = "/showPublicBoards";
+		public static final String viewPublicBoardMapping = "/viewPublicBoard";
+		public static final String dashboardMapping = "/dashboard";
+		public static final String viewBoardMapping = "/viewBoard";
+		public static final String deleteBoardMapping = "/deleteBoard";
+		public static final String deleteLinkMapping = "/deleteLink";
+		public static final String createLinkMapping = "/createLink";
+		public static final String createBoardMapping = "/createBoard";
+		public static final String welcomeMapping = "/welcome";
+		public static final String loginfailedMapping = "/loginfailed";
+		public static final String logoutMapping = "/logout";
+		public static final String loginMapping = "/login";
+			
+		
+		// Return Pages
+		public static final String registerPage = "register.jsp";
+		public static final String adminPage = "admin.jsp";
+		public static final String publicBoardsPage = "publicBoards.jsp";
+		public static final String publicBoardPage = "publicBoard.jsp";
+		public static final String dashboardPage = "dashboard.jsp";
+		public static final String boardPage = "board.jsp";
+		public static final String indexPage = "index.jsp";
+					
+		// Model vars & messages
+		public static final String error = "error";
+		public static final String message = "message";
+		public static final String users = "users";
+		public static final String currentUser = "currentUser";
+		public static final String publicBoards = "publicBoards";
+		public static final String privateBoards = "privateBoards";
+		public static final String links = "links";
+		public static final String board = "board";
+		public static final String boards = "boards";
+		public static final String dashboard = "dashboard";
+		public static final String registrationSuccess = "Registration successful! Please login.....";
+		public static final String namePasswordMandatory = "Name & Password are mandatory!!";
+		public static final String nameMandatory = "Name is mandatory!!";
+		public static final String passwordsDoNotMatch = "Passwords do not match!!";
+		public static final String userAlreadyExists = "User already exists!!";
+}
